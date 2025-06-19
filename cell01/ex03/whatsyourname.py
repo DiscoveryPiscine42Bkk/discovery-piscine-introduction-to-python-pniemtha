a9c3836c61ba76713d98372c49654ba1515b979e
pniemtha <pornpimon.004.p@gmail.com>
2025-06-19 03:11:29 +0000

    am

--- cell01/ex03/whatsyourname.py
+++
@@ -1,3 +0,0 @@
-first_name = input("Hey, what's your first name? : Piyawan")
-last_name = input("And your last name? : Keawsri")
-print("Well, pleased to meet you,", Piyawan, Keawsri)
---
+++ cell01/ex03/whatyourname.py
@@ -0,0 +1,3 @@
+first_name = input("Hey, what's your pornpimol : ")
+last_name = input("And your niemtham : ")
+print("Well, pleased to meet you,", pornpimol, niemtham)
